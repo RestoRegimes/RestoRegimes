d60ce30b2434a8ff66dba0f202256a6ea30e5dc0
LFBarreto <lfb.access@gmail.com>
2015-12-06 17:59:30 +0100

    correctif bis ajout du formulaire de recherche de base

--- src/RR/CoreBundle/Controller/DefaultController.php
+++ src/RR/CoreBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace RR\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller
 {
@@ -10,8 +12,43 @@ class DefaultController extends Controller
     {
         return $this->render('RRCoreBundle:Default:layout.html.twig',array());
     }
-    public function rechercheAction()
+    public function rechercheAction(Request $request)
     {
-        return $this->render('RRCoreBundle:Default:search.html.twig',array());
+
+        $data = array();
+        $form = $this->createFormBuilder($data)
+            ->add('recherche', 'text')
+            ->add('vegetarien', 'checkbox', array(
+                'label'    => 'Vegetarien',
+                'required' => false,
+            ))
+            ->add('vegetalien', 'checkbox', array(
+                'label'    => 'Vegetalien',
+                'required' => false,
+            ))
+            ->add('gluten', 'checkbox', array(
+                'label'    => 'Sans gluten',
+                'required' => false,
+            ))
+            ->add('diabete', 'checkbox', array(
+                'label'    => 'Diabete',
+                'required' => false,
+            ))
+            ->add('Cholesterol', 'checkbox', array(
+                'required' => false,
+            ))
+            ->add('rechercher','submit')
+            ->getForm();
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+
+        // $data is a simply array with your form fields
+        // like "query" and "category" as defined above.
+        $data = $form->getData();
+    }
+        return $this->render('RRCoreBundle:Default:search.html.twig',array(
+            "form"=>$form->createView()
+        ));
     }
 }
