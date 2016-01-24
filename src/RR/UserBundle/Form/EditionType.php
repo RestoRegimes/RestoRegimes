c56b266e21f58e751c84bc44752e7b6c7f6fbf69
LFBarreto <lfb.access@gmail.com>
2016-01-24 17:58:09 +0100

    update edition de profil en cours

---
+++ src/RR/UserBundle/Form/EditionType.php
@@ -0,0 +1,44 @@
+<?php
+namespace RR\UserBundle\Form;
+
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+use RR\CoreBundle\Form\AdresseType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\CallbackTransformer;
+
+class EditionType extends BaseType
+{
+public function buildForm(FormBuilderInterface $builder,
+array $options)
+{
+    parent::buildForm($builder, $options);
+    $builder->add('address',new AdresseType(),array(
+        'label' => ' ',
+        'data_class' => 'Padam87\AddressBundle\Entity\GeocodedAddress',
+        'required'=>false
+    ))
+        ->add($builder->create('telephone','text',array('required'=>false))
+            ->addModelTransformer(
+                new CallbackTransformer(
+                // transform <br/> to \n so the textarea reads easier
+                    function ($originalDescription) {
+
+                        return preg_replace("/[0-9]{2}/", "$0 ", $originalDescription);
+                    },
+                    function ($submittedDescription) {
+                        return preg_replace('/[-. ]/', "",$submittedDescription);
+                    }
+                )
+            ))
+    ->add('profileImage',new UserImageType(),array('required'=>false))
+    ->add('recaptcha', 'ewz_recaptcha');
+
+
+
+}
+
+public function getName()
+{
+return 'rruserbundle_user_edition';
+}
+}
